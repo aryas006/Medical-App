83edc22944e11a6f6521430d6164c1179c3656bf
Arya <128399498+aryas006@users.noreply.github.com>
2024-03-06 20:14:53 +0530

    Initial commit

---
+++ lib/pages/Login.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/pages/SignUp.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Sign Up',
+    home: Login(),
+  ));
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 195, 215),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/meditation.png',
+                width: 200.0,
+                height: 200.0,
+              ),
+              const SizedBox(height: 16.0),
+              const TextField(
+                // ignore: unnecessary_const
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your signup logic here
+                },
+                child: const Text('Log In'),
+              ),
+              const SizedBox(height: 16.0),
+              TextButton(
+                onPressed: () {
+ Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUp()),
+                    );
+                },
+                child: const Text('Dont have an account? Click here to SignUp'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/Survey.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+
+class Intro extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    )}}
